8ee2d90b84e1d645f6360d1dd4fe08e0369dbbe2
Krish Bhuva <bhuvakrish03@gmail.com>
2025-04-11 23:29:46 +0530

    Add files via upload

--- client/App.js
+++ client/App.js
@@ -12,7 +12,7 @@ import Bookings from "./pages/Bookings";
 import background from "./utils/Images/Background.svg";
 import Contact from "./pages/Contact";
 import Blogs from "./pages/Blogs";
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights"
 import { Analytics } from "@vercel/analytics/react"
 import BlogDetails from "./pages/BlogDetails";
 import Favorites from "./pages/Favorites";
