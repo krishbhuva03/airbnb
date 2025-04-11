41c89b0a3a918ec9321e51d5dbb0740159d07b40
Krish Bhuva <bhuvakrish03@gmail.com>
2025-04-11 16:07:11 +0530

    Add files via upload

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "https://server-eta-sable.vercel.app/api/",
 })
 
 export const UserSignUp = async (data) => await API.post("user/signup", data)
