ea8ea34b6f0a5eadc5267300b8e345f16a0e55a1
Krish Bhuva <bhuvakrish03@gmail.com>
2025-12-30 16:56:37 +0530

    fix: Case-sensitivity bug in User model import - fixes single sign-in on Linux servers

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import { createError } from "../error.js";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 export const verifyToken = async (req, res, next) => {
   try {
